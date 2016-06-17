7faf9bb2c3252342c4fe767405ebf19aa8f198a6
cac7usman <a.shumil@gmail.com>
2016-06-17 08:19:55 +0300

    17.6

---
+++ src/loops/PrimeNumber.java
@@ -0,0 +1,7 @@
+package loops;
+
+/**
+ * Created by Alex on 14.06.2016.
+ */
+public class PrimeNumber {
+}
---
+++ src/operators/Rating.java
@@ -0,0 +1,7 @@
+package operators;
+
+/**
+ * Created by Alex on 14.06.2016.
+ */
+public class Rating {
+}
